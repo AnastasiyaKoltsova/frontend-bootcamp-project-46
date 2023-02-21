3b3c935009657ad6c8489fa24d612c3a799831c1
Anastasiya Koltsova <Koltsova_av@mail.ru>
2023-02-21 08:57:05 +0300

    update gendiff, add arguments and options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,9 @@
 import { program } from 'commander';
 
 program
-    .name('string-util')
-    .description('CLI to some JavaScript string utilities')
-    .version('0.8.0');
+    .description('Compares two configuration files and shows a difference.')
+    .version('0.1.0')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format <type>', 'output format');
 
 program.parse();
