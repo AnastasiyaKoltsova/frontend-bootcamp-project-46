8f380184203261243ed7725516acf6aedb0e1ba9
Anastasiya Koltsova <Koltsova_av@mail.ru>
2023-02-28 14:58:20 +0300

    fix

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -13,13 +13,17 @@ const resultPlain = fs.readFileSync(
   'utf-8',
 );
 
+// eslint-disable-next-line no-undef
 test.each(fileExt)('testing different file options', (extension) => {
   const fileBefore = `__fixtures__/file1${extension}`;
   const fileAfter = `__fixtures__/file2${extension}`;
   const actual1 = genDiff(fileBefore, fileAfter, 'stylish');
+  // eslint-disable-next-line no-undef
   expect(actual1).toEqual(resultStylish);
   const actual2 = genDiff(fileBefore, fileAfter, 'plain');
+  // eslint-disable-next-line no-undef
   expect(actual2).toEqual(resultPlain);
   const actual4 = genDiff(fileBefore, fileAfter);
+  // eslint-disable-next-line no-undef
   expect(actual4).toEqual(resultStylish);
 });
