9a6ea209e6b5bffea5f6eaf234f6cb3955043902
Anastasiya Koltsova <Koltsova_av@mail.ru>
2023-02-28 15:46:34 +0300

    fix bugs

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import { expect, test } from '@jest/globals';
 import genDiff from '../src/index.js';
 
 const fileExt = ['.json', '.yml'];
@@ -13,17 +14,15 @@ const resultPlain = fs.readFileSync(
   'utf-8',
 );
 
-// eslint-disable-next-line no-undef
 test.each(fileExt)('testing different file options', (extension) => {
   const fileBefore = `__fixtures__/file1${extension}`;
   const fileAfter = `__fixtures__/file2${extension}`;
   const actual1 = genDiff(fileBefore, fileAfter, 'stylish');
-  // eslint-disable-next-line no-undef
   expect(actual1).toEqual(resultStylish);
   const actual2 = genDiff(fileBefore, fileAfter, 'plain');
-  // eslint-disable-next-line no-undef
+  console.log('str1', actual2.split());
+  console.log('str2', resultPlain.split());
   expect(actual2).toEqual(resultPlain);
   const actual4 = genDiff(fileBefore, fileAfter);
-  // eslint-disable-next-line no-undef
   expect(actual4).toEqual(resultStylish);
 });
